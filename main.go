6cacb6a64ba8dfadf3867441d43cd5f656bf664e
Walter Stahll <stahllw@ufl.edu>
2023-02-08 20:46:36 -0500

    Event struct, insert function, & "time" package

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
@@ -13,6 +14,24 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type Event struct {
+	EventName   string
+	DTCreate    string // Date & Time that Event was created
+	DTStart     string // Date & Time that Event is scheduled to start
+	DTEnd       string // Date & Time that Event is scheduled to end
+	CreatorName string // Event Creator's ID
+	CreatorID   string // Event Creator's ID
+	EventDesc   string // Event Description
+}
+
+func insertEvent(collection *mongo.Collection, newEvent Event) {
+	insertResult, err := collection.InsertOne(context.TODO(), newEvent)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted a single document:", insertResult.InsertedID)
+}
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
@@ -86,6 +105,20 @@ func main() {
 	}
 	// display the ids of the newly inserted objects
 	fmt.Println(results.InsertedIDs)
+
+	// testing the insertEvent function
+	currentTime := time.Now() // gets the current time
+	startTime := time.Date(currentTime.Year(), time.March, 11, 0, 0, 0, 0, currentTime.Location())
+	endTime := time.Date(currentTime.Year(), time.March, 19, 23, 59, 59, 0, currentTime.Location())
+	newEvent := Event{EventName: "Spring Break", DTCreate: currentTime.Format("2006-01-02 15:04:05"), DTStart: startTime.Format("2006-01-02 15:04:05"), DTEnd: endTime.Format("2006-01-02 15:04:05"), CreatorName: "stahllw", CreatorID: "1", EventDesc: "2023 Spring Break! We have no school!"}
+	eventsCollection := client.Database("testing").Collection("events")
+	err = eventsCollection.Drop(context.TODO())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Collection dropped successfully!")
+	insertEvent(eventsCollection, newEvent)
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
