823e7bc05ccd6a3453ad19dce67346fdf5ea26b8
Kate Schwitz <katiejoschwitz@gmail.com>
2023-03-01 02:16:42 -0500

    again

---
+++ src/app/loginPage/loginPage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginPage',
+  templateUrl: './loginPage.component.html',
+  styleUrls: ['./loginPage.component.css']
+})
+export class LoginPageComponent {
+
+}
