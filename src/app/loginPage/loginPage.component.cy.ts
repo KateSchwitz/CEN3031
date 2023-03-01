04497b4ae86200c355085f7ac9b87039fe220c2b
Kate Schwitz <katiejoschwitz@gmail.com>
2023-03-01 14:28:13 -0500

    adding loginPage component cypress test

---
+++ src/app/loginPage/loginPage.component.cy.ts
@@ -0,0 +1,7 @@
+import { LoginPageComponent } from './loginPage.component'
+
+describe('LoginPageComponent', () => {
+  it('mounts', () => {
+    cy.mount(LoginPageComponent)
+  })
+})
--- src/app/loginPage/loginPage.component.ts
+++ src/app/loginPage/loginPage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-loginPage',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./loginPage.component.css']
 })
 export class LoginPageComponent {
+  constructor() {
+    
+  }
 
+  ngOnInit(): void {}
+
+  @Input() count = 0
+  @Output() change = new EventEmitter()
+  @Output() username = new String()
+  @Output() password = new String()
+
+  increment(): void {
+    this.count++
+    this.change.emit(this.count)
+  }
+
+  decrement(): void {
+    this.count--
+    this.change.emit(this.count)
+  }
+
+  save():void{
+      this.username = "Kate"
+      this.password = "aaaaa"
+  }
 }
